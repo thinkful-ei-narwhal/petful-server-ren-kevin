33aca08a3f1721e48e3021b9a93ffb9a9b9b173d
Kevin Jiang <kjiang95@gmail.com>
2020-06-19 13:14:12 -0500

    finished endpoints

--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -1,23 +1,57 @@
+const _Node = require('./node');
 class Queue {
   constructor() {
-    // Set initial data.
+    this.first = null;
+    this.last = null;
   }
 
   enqueue(data) {
-    // Add some data to the queue.
+    const node = new _Node(data);
+
+    if (this.first === null) {
+      this.first = node;
+    }
+
+    if (this.last) {
+      this.last.next = node;
+    }
+
+    this.last = node;
   }
 
   dequeue() {
-    // Remove some data from the queue.
+    if (this.first === null) {
+      return;
+    }
+
+    const node = this.first;
+    this.first = this.first.next;
+
+    if (node === this.last) {
+      this.last = null;
+    }
+    return node.value;
   }
 
   show() {
-    // Return the next item in the queue.
+    if (this.first === null) {
+      console.log('nothing in queue');
+    }
+    return this.first;
   }
 
   all() {
-    // Return all items in the queue.
+    let temp= this.first;
+    let all = [];
+    if (temp === null) {
+      console.log('nothing in stack');
+    } 
+    while(temp){//if ticket ask (temp !==null) not work
+      all.push(temp.value);
+      temp = temp.next;
+    }
+    return all;
   }
 }
 
-module.exports = Queue
+module.exports = Queue;
